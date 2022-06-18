37c12b113acb673be86e7fb94bb67dbfb6e27562
nori3636 <nori.k3636@gmail.com>
2022-06-19 02:56:43 +0900

    :bug: crlf to lf

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,18 @@ client.once('ready', () => {
 	console.log('ready?');
 });
 
+client.on('messageCreate', async (message) => {
+	if (message.author.bot) {
+		return; // Botからの通信だった場合は無視
+	}
+	console.log(`[${message.author.tag}]さんから ${message.content}と言われました`);
+	if (!client.user) {
+		return;
+	}
+	if (message.mentions.users.has(client.user.id)) {
+		message.reply('こんにちは！');
+		return;
+	}
+});
+
 client.login(process.env.TOKEN);
